d1f8df7d8450b8f4c9a8057446c4980439d97b23
CatrielZ <95256921+CatrielZ@users.noreply.github.com>
2022-06-28 20:29:11 -0300

    error

--- Backend Clase 13/Pug/routes/index.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-const router = Router()
-import { getProductos, postProductos, mostrarForm  } from '../controllers/productosController';
-
-router.get('/productos', getProductos)
-router.get('/crear', mostrarForm)
-router.post('/productos', postProductos)
-
-export default router
